ecd6c94420db099b51e9709ad73751a7f408599e
Olivia Rojas <olivia.chisman@gmail.com>
2022-02-12 23:18:35 -0600

    finished hw 5

--- Shopping List/ViewController.swift
+++ Shopping List/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
     var itemDescription: String = ""
     var itemQuantity: String = ""
 
+    @IBOutlet weak var descriptionField: UITextField!
+    @IBOutlet weak var quantityField: UITextField!
     @IBOutlet weak var itemsView: UILabel!
     
     override func viewDidLoad() {
@@ -20,12 +22,49 @@ class ViewController: UIViewController {
     }
 
     @IBAction func newListPressed(_ sender: UIButton) {
+        clearItemFields()
+        itemsView.text = "No items"
     }
     
     @IBAction func newItemPressed(_ sender: UIButton) {
+        clearItemFields()
+    }
+    
+    func clearItemFields() {
+        descriptionField.text = ""
+        quantityField.text = ""
+        itemDescription = ""
+        itemQuantity = ""
     }
     
     @IBAction func addPressed(_ sender: UIButton) {
+        if (itemDescription.isEmpty) {
+            displayInvalidEntryPopup(
+                title: "Missing Item Description",
+                message: "Please enter a description for your item")
+            return
+        }
+        
+        if let quantity = Int(itemQuantity) {
+            if (quantity != 0) {
+                addNewItemToList(quantity)
+                return
+            }
+        }
+        
+        displayInvalidEntryPopup(
+            title: "Missing Item Quantity",
+            message: "Please enter a valid quantity for your item")
+    }
+    
+    func addNewItemToList(_ quantity: Int) {
+        let newLine: String = "\(quantity) \(itemDescription)\n"
+        if (itemsView.text == nil || itemsView.text == "No items") {
+            itemsView.text = newLine
+        } else {
+            itemsView.text!.append(newLine)
+        }
+        clearItemFields()
     }
     
     @IBAction func descriptionEntered(_ sender: UITextField) {
@@ -38,6 +77,21 @@ class ViewController: UIViewController {
         if let text = sender.text {
             itemQuantity = text
         }
+    }
+    
+    @IBAction func editEnded(_ sender: UITextField) {
+        sender.resignFirstResponder()
+    }
+    
+    @IBAction func numericalEditEnded(_ sender: UIControl) {
+        quantityField.resignFirstResponder()
+    }
+    
+    func displayInvalidEntryPopup(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true, completion: nil)
     }
 }
 
