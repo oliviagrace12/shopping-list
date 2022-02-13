e0b7cbd3aed6b95e710778d4dc0d3d29a6b1b8e3
Olivia Rojas <olivia.chisman@gmail.com>
2022-02-12 22:10:57 -0600

    initial commit

--- Shopping List/ViewController.swift
+++ Shopping List/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var itemDescription: String = ""
+    var itemQuantity: String = ""
 
+    @IBOutlet weak var itemsView: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func newListPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func newItemPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func addPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func descriptionEntered(_ sender: UITextField) {
+        if let text = sender.text {
+            itemDescription = text
+        }
+    }
+    
+    @IBAction func quantityEntered(_ sender: UITextField) {
+        if let text = sender.text {
+            itemQuantity = text
+        }
+    }
 }
 
